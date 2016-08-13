fc89499064783a533834aec623aefb7ded23ac7c
rahulkamra <rahulkamra@gmail.com>
2016-08-13 14:03:05 +0400

    Renamed 2,3,4,5
    Renamed 2,3,4,5

---
+++ HelloUnity/Assets/Chapter05StaticFields/Chap5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chapter1;
+using UnityEngine;
+namespace Chapter5
+{
+    class Chap5
+    {
+        //static classes
+        public void execute()
+        {
+            
+
+            Debug.Log(ClassA._classB.myName);
+        }
+    }
+    
+}
